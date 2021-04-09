0b53ebc6ba3808411afdbd7f51c01c0d2b1b31e3
URAN <urecha@emea.roche.com>
2021-04-09 09:05:25 +0200

    initial commit

---
+++ ch.gibz.m151.uran.data/PhotoGalleryDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ch.gibz.m151.uran.data
+{
+    public class PhotoGalleryDb : DbContext
+    {
+    }
+}
